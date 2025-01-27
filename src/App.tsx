a801ce93d785ee8fe89191a9963af890e624e6cf
calvocc <calvocreativocolombiano@gmail.com>
2025-01-27 14:53:27 -0500

    sistema de rutas configurado

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Routes, Route, useNavigate } from "react-router-dom";
 import { AuthContext } from "./context/auth-context";
 import RequireAuth from "./components/require-auth";
 import Home from "./routes/home";
+import Login from "./routes/login";
 import Profile from "./routes/profile";
 
 function App() {
@@ -21,6 +22,7 @@ function App() {
   return (
     <Routes>
       <Route index element={<Home />} />
+      <Route path="login" element={<Login />} />
       <Route
         path="profile"
         element={
